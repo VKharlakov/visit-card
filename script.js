4e4ad5164fff6407390954ee4f121732bab35022
Vitaly Kharlakov <vkharlakov@yandex.ru>
2022-10-05 16:39:59 +0200

    Исправлена грид-разметка карточек, измененена анимация с hover>click, написан код в js с этой анимацией

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ const form = document.querySelector('.form')
 const submitBtn = document.querySelector('.form__button')
 const formContainer = document.querySelector('.login')
 const greetMsg = document.querySelector('#greeting')
+const infoCardList = Array.from(document.querySelectorAll('.bio__item'))
+
+infoCardList.forEach(function(cardElement) {
+    cardElement.addEventListener('click', function(event) {
+        const cardText = cardElement.querySelector('.bio__item-text')
+        const cardIcon = cardElement.querySelector('.bio__item-icon')
+        const cardClickIcon = cardElement.querySelector('.bio__item-clickIcon')
+
+        cardIcon.style.bottom = '80%'
+        cardText.style.visibility = 'visible'
+        cardText.style.opacity = '1'
+        cardClickIcon.style.opacity = '0'
+        cardClickIcon.style.visibility = 'hidden'
+    })
+})
+
+
 
 function greetUser() {
     greetMsg.textContent = `${document.querySelector('#inputName').value}  ${document.querySelector('#inputSurname').value}`
